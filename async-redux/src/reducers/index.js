b3edba6640d220ec8da10ed1b51c9ed3c62ed6dd
William Schwindt <williammschwindt@gmail.com>
2020-03-03 11:46:32 -0800

    launch details rendered on click

--- async-redux/src/reducers/index.js
+++ async-redux/src/reducers/index.js
@@ -1,9 +1,30 @@
 import { FETCHING_LAUNCH_START, FETCHING_LAUNCH_SUCCESS, FETCHING_LAUNCH_FAILURE } from '../actions/index';
 
-const initialState = {}
+const initialState = {
+    launchDetails: {},
+    isFetching: false,
+    error: ''
+}
 
 export const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCHING_LAUNCH_START :
+            return {
+                ...state,
+                isFetching: true
+            }
+        case FETCHING_LAUNCH_SUCCESS :
+            return {
+                ...state,
+                launchDetails: action.payload,
+                isFetching: false
+            }
+        case FETCHING_LAUNCH_FAILURE :
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
+            }
         default :
             return state;
     }
